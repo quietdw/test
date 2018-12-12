c22d419fb602fc01d75d623289d7e2ff9e5c1981
quietdw <1044569102@qq.com>
2018-12-12 10:39:16 +0800

    swipe-demo js修改

--- swipe-demo/main.js
+++ swipe-demo/main.js
@@ -1,37 +1,35 @@
-window.onload=function(){
-    var $buttons = $('.buttonList>span')
-    var $imgs = $('.images>img')
-    var n=0
-    var size = $imgs.length
+var $buttons = $('.buttonList>span')
+var $imgs = $('.images>img')
+var n=0
+var size = $imgs.length
 
-    for(let i=0;i<$buttons.length;i++){
-        $($buttons[i]).on('click',function(e){
-            let $index = $(e.currentTarget).index()
-            n=$index
-            $(e.currentTarget).addClass('active').siblings('.active').removeClass('active')
-            
-            $('.images').css(
-                'transform','translateX(-'+ $index*1048 +'px)'
-            )
-        })
-    }
-
-    var timer = setTimer.call()
-
-    $('.container').on('mouseenter',function(){
-        window.clearInterval(timer)
+for(let i=0;i<$buttons.length;i++){
+    $($buttons[i]).on('click',function(e){
+        let $index = $(e.currentTarget).index()
+        n=$index
+        $(e.currentTarget).addClass('active').siblings('.active').removeClass('active')
+        
+        $('.images').css(
+            'transform','translateX(-'+ $index*1048 +'px)'
+        )
     })
-
-    $('.container').on('mouseleave',function(){
-        timer = setTimer.call()
-    })
-
-    function setTimer(){
-        return  setInterval(function(){
-            n+=1
-            $($buttons[n%size]).trigger('click')
-            console.log(n%size)
-        },3000)
-    }
-
 }
+
+var timer = setTimer.call()
+
+$('.container').on('mouseenter',function(){
+    window.clearInterval(timer)
+})
+
+$('.container').on('mouseleave',function(){
+    timer = setTimer.call()
+})
+
+function setTimer(){
+    return  setInterval(function(){
+        n+=1
+        $($buttons[n%size]).trigger('click')
+        console.log(n%size)
+    },3000)
+}
+
