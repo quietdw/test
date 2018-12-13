a07f394e11a414447d01f46d0cc6b28029e0405e
quietdw <1044569102@qq.com>
2018-12-13 08:52:33 +0800

    更换目录，demo-2无缝轮播

---
+++ swipe-demo/demo2/main.js
@@ -0,0 +1,18 @@
+var $images = $('.images>img')
+var n=0
+//初始化
+$images.eq(0).addClass('current').siblings().addClass('enter')
+
+var timer = setInterval(function(){
+    n=getN(n)
+    $images.eq(n).removeClass('current').addClass('leave').one('transitionend',function(e){
+       $(e.currentTarget).removeClass('leave').addClass('enter')
+    })
+    
+    $images.eq(getN(n+1)).removeClass('enter').addClass('current')
+    n+=1
+},3000)
+
+function getN(n){
+    return n>=$images.length?n=0:n
+}
