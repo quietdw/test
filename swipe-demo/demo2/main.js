06639429ce755b210f4b9e89162556f6ec1cb2a6
quietdw <1044569102@qq.com>
2018-12-18 09:53:30 +0800

    增加visibilitychange事件

--- swipe-demo/demo2/main.js
+++ swipe-demo/demo2/main.js
@@ -13,6 +13,22 @@ var timer = setInterval(function(){
     n+=1
 },3000)
 
+document.addEventListener('visibilitychange',function(){
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval(function(){
+            n=getN(n)
+            $images.eq(n).removeClass('current').addClass('leave').one('transitionend',function(e){
+               $(e.currentTarget).removeClass('leave').addClass('enter')
+            })
+            
+            $images.eq(getN(n+1)).removeClass('enter').addClass('current')
+            n+=1
+        },3000)
+    }
+})
+
 function getN(n){
     return n>=$images.length?n=0:n
 }
